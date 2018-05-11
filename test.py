95ee740abcd93aca72104840b784672e4a2303ad
Nicholas Marinakis <nakis@apple.com>
2018-05-10 18:02:58 -0700

    Remove StringIO call

--- test.py
+++ test.py
@@ -38,7 +38,6 @@ class TestClass(object):
         """
         with open(json_file_path) as f:
             data = json.load(f)
-        fp = StringIO()
         with open(outfile_path, 'w') as fp:
             writer = csv.writer(fp)
             writer.writerow(data[0].keys())
